569d49ab80c0663522a5595f9a81e7cf3c8445b4
Rostislav Klein <rostislav.klein@dhl.com>
2018-04-06 17:12:58 +0200

    Heroku setup.

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://'+process.env.DBUSER+':'+process.env.DBPASS+'@ds129428.mlab.com:29428/secure-api-test');
+mongoose.connect('mongodb://'+process.env.DBUSER+':'+process.env.DBPASS+'@'+process.env.DB);
