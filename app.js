2242dca70f840a50e9752ed263f9ee1387ba0542
Rostislav Klein <rostislav.klein@dhl.com>
2018-04-06 21:30:57 +0200

    force ssl on production

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var app = express();
 var db = require('./db');
 const cors = require('cors');
 const bodyParser = require("body-parser");
-var cookieParser = require('cookie-parser')
+var cookieParser = require('cookie-parser');
+var enforce = require('express-sslify');
+
+
+const dev = process.env.NODE_ENV !== 'production'
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(cookieParser())
 
+app.use(cookieParser())
 app.use(cors());
 app.options('*', cors());
+
+if(!dev) app.use(enforce.HTTPS());
 
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
