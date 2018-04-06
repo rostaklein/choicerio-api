1d3bee74e3a2650c1df24eba0f71f3cb45f80461
Rostislav Klein <rostislav.klein@dhl.com>
2018-04-06 21:35:11 +0200

    try without ssl

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var db = require('./db');
 const cors = require('cors');
 const bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
-var enforce = require('express-sslify');
+//var enforce = require('express-sslify');
 
 
-const dev = process.env.NODE_ENV !== 'production'
+//const dev = process.env.NODE_ENV !== 'production'
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(cors());
 app.options('*', cors());
 
-if(!dev) app.use(enforce.HTTPS());
+//if(!dev) app.use(enforce.HTTPS());
 
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
