49b2e1f3e32de52276c39a8667c26dd3eb0f5236
Rostislav Klein <rostislav.klein@dhl.com>
2018-04-09 12:41:07 +0200

    Verify token status changes.

--- auth/VerifyToken.js
+++ auth/VerifyToken.js
@@ -4,10 +4,10 @@ var config = require('../config');
 function verifyToken(req, res, next) {
   // req.headers.cookie && cookie.parse(req.headers.cookie).token;
   var token = req.headers['x-access-token'];
-  if (!token) return res.status(403).send({ auth: false, msg: 'No token provided.' });
-    jwt.verify(token, config.secret, function(err, decoded) {
+  if (!token) return res.status(400).send({ auth: false, msg: 'No token provided.' });
+  jwt.verify(token, config.secret, function(err, decoded) {
     if (err)
-    return res.status(500).send({ auth: false, msg: 'Failed to authenticate token.' });
+    return res.status(401).send({ auth: false, msg: 'Failed to authenticate token.' });
     // if everything good, save to request for use in other routes
     req.userId = decoded.id;
     next();
